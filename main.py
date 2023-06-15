13c4898ca7b134edef4bebfff6da4c59b626d8d8
Saptarshi Mandal <saptarshim925whjr@gmail.com>
2023-06-15 19:05:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+from flask import Flask, request, redirect, url_for, send_file, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route("/")
+def upload_form():
+    return render_template("upload.html")
+
+@app.route("/gray", methods=["POST"])
+def upload_video():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join('static/', filename))
+    return render_template("upload.html", filename=filename)
+
+@app.route("/display/<filename>")
+def display_video(filename):
+    return redirect(url_for('static', filename=filename))
+
+if __name__ == "__main__":
+    app.run()
