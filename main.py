623d66a48212cd458e23226cbe9f951cad4013aa
Saptarshi Mandal <saptarshim925whjr@gmail.com>
2023-07-22 18:41:20 +0530

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import os
 import cv2
-from flask import Flask, request, redirect, url_for, send_file, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def upload_form():
-    return render_template("upload.html")
+    return render_template('upload.html')
 
-@app.route("/gray", methods=["POST"])
+@app.route('/', methods=['POST'])
 def upload_video():
     file = request.files['file']
     filename = secure_filename(file.filename)
     file.save(os.path.join('static/', filename))
-    return render_template("upload.html", filename=filename)
+    source = cv2.VideoCapture('static/' + filename) 
+    frame_width = int(source.get(3))
+    frame_height = int(source.get(4))
+    size = (frame_width, frame_height)
+    result = cv2.VideoWriter('static/' + 'blackandwhite.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, size, 0)
+    try:
+        while True:
+            status, frame_image = source.read()
+            gray = cv2.cvtColor(frame_image, cv2.COLOR_RGB2GRAY)
+            result.write(gray)
+            video_file = 'blackandwhite.mp4'
+    except:
+        print('Completed reading all the Frames from the Video')
+    return render_template('upload.html', filename=filename)
 
-@app.route("/display/<filename>")
+@app.route('/download')
+def download_file():
+    converted_video_path = 'static/blackandwhite.mp4'
+    return send_file(converted_video_path, as_attachment=True)
+
+@app.route('/display/<filename>')
 def display_video(filename):
     return redirect(url_for('static', filename=filename))
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
